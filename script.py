82cf1a841e3fc184e1bba25a4f93e506b9178422
Kabilan S <kabilansakthivel1290@gmail.com>
2024-11-06 13:41:20 +0530

    script added v1

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from mitmproxy import http, ctx
+from urllib.parse import urlparse
 
 class CORSchecker:
     def replay_with_different_origins(self, original_flow, origins):
@@ -25,10 +26,12 @@ class CORSchecker:
         # Check if the flow is a replay request
         if flow.is_replay:
             return
+        parsed_url = urlparse(flow.request.url)
+        hostname_vaue = parsed_url.hostname
         cors1 = "https://evil.com"
-        cors2 = flow.request.url[:-1]+".evil.com"
-        cors3 = flow.request.url[:-1]+"evil.com"
-        cors4 = "evil"+flow.request.url[:-1]
+        cors2 = "https://"+hostname_vaue+".evil.com"
+        cors3 = "https://"+hostname_vaue+"evil.com"
+        cors4 = "https://"+"evil"+hostname_vaue
         cors5 = "null"
         # Define the list of different origins
         origins = [cors1, cors2, cors3, cors4, cors5]
@@ -39,21 +42,38 @@ class CORSchecker:
 
     def response(self, flow: http.HTTPFlow) -> None:
         try:
+            parsed_url = urlparse(flow.request.url)
+            hostname_vaue = parsed_url.hostname
             # Check CORS-related issues in the response
-            if "https://evil.com" in flow.response.headers["access-control-allow-origin"]:
+            if "evil.com" in flow.response.headers["access-control-allow-origin"]:
                 print("--------[+] CORS issue: Accept any domain--------")
                 print("[+] Reflected Origin: " + flow.request.url)
                 if "true" in flow.response.headers["access-control-allow-credentials"]:
                     print("[+] Credentials Enabled")
                     print(flow.request.url)
-            if flow.request.url in flow.response.headers["access-control-allow-origin"]:
-                print("--------[+] CORS issue: CORSdomain Bypass--------")
+            if "https://"+hostname_vaue+".evil.com" in flow.response.headers["access-control-allow-origin"]:
+                print("--------[+] CORS issue: CORS domain Bypass--------")
                 print("[+] Reflected Origin: " + flow.request.url)
                 if "true" in flow.response.headers["access-control-allow-credentials"]:
                     print("[+] Credentials Enabled")
+            if "https://"+hostname_vaue+"evil.com" in flow.response.headers["access-control-allow-origin"]:
+                print("--------[+] CORS issue: CORS Suffix Bypass--------")
+                print("[+] Reflected Origin: " + flow.request.url)
+                if "true" in flow.response.headers["access-control-allow-credentials"]:
+                    print("[+] Credentials Enabled")
+            if "https://"+"evil"+hostname_vaue in flow.response.headers["access-control-allow-origin"]:
+                print("--------[+] CORS issue: CORS prefix Bypass--------")
+                print("[+] Reflected Origin: " + flow.request.url)
+                if "true" in flow.response.headers["access-control-allow-credentials"]:
+                    print("[+] Credentials Enabled")
+            if "null" in flow.response.headers["access-control-allow-origin"]:
+                print("--------[+] CORS issue: CORS null Bypass--------")
+                print("[+] Reflected Origin: " + flow.request.url)
+                if "true" in flow.response.headers["access-control-allow-credentials"]:
+                    print("[+] Credentials Enabled")                       
             else:
-                print("--------[-] No CORS issue--------")
+                pass
         except Exception as e:
-            print(f"Error processing response: {e}")
+            pass
 
 addons = [CORSchecker()]
